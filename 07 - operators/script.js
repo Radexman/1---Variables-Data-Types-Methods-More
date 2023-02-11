0ae34f8c69954fad451beb675c731e20f1017031
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2023-02-11 22:44:25 +0100

    02.11

---
+++ 07 - operators/script.js
@@ -0,0 +1,50 @@
+// Artihmetic Operators
+let x;
+
+x = 5 + 5; // Addition
+x = 5 - 5; // Substraction
+x = 5 * 5; // Multiplication
+x = 5 / 5; // Division
+x = 7 % 5; // Modulo (reminder from division)
+
+// Concatenation
+x = 'Hello' + ' ' + 'World';
+
+// Exponent
+x = 2 ** 3;
+
+// Increment
+x = 1;
+// x = x + 1;
+// x++;
+
+// Decrement
+// x = x - 1;
+x--;
+
+// Assignment Operators
+x = 10;
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+
+// Comparison Operators
+// Single = sign is asingment
+// Double == evaluating an expression, comparison operator
+// Triple === evaluating value and type
+x = 1 == 1;
+
+x = 2 === '2';
+
+x = 2 != '2';
+
+x = 2 !== 2;
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
