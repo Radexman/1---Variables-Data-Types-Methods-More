ec1183452463fb3759954effb1ca7408ced87e47
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2023-02-11 12:10:20 +0100

    02.11

---
+++ 02 - comments and shortcuts/script.js
@@ -0,0 +1,23 @@
+// This is single line of code
+console.error('Something is wrong!');
+console.error('Something is wrong!');
+console.error('Something is wrong!');
+
+console.warn('This is your last warning');
+const person = {
+	name: 'Radek',
+	age: 23,
+};
+const styles = 'padding: 10px; background-color: white; color: green;';
+console.warn('%cHello', styles);
+
+console.table(person);
+console.warn(x);
+console.group('simple');
+console.warn('Hello!');
+console.warn('Hello!');
+console.warn('Hello!');
+console.warn('Hello!');
+console.warn('Radek', 23, false);
+let x = 100;
+console.error('simple');
