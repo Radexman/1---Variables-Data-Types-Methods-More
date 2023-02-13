03dbb998549ec82d59ab717ff1e35d8bee96dae5
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2023-02-13 22:53:18 +0100

    02.13

---
+++ 09 - strings/script.js
@@ -0,0 +1,19 @@
+let x;
+
+const name = 'Radek';
+const age = 23;
+
+x = 'Hello, my name is ' + name + " and I'am " + age + ' years old.';
+
+// Template Literals
+
+x = `Hello my name is ${name} and I'am ${age} years old.`;
+
+// String Properties and Methods
+const s = new String('Hello World');
+
+x = typeof s;
+
+// x = s.length;
+
+console.log(x);
