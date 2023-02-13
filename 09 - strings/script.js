eaa89bd57dfc89ab34913cad4ac13f1e18fdb8f5
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2023-02-13 23:13:43 +0100

    02.13

--- 09 - strings/script.js
+++ 09 - strings/script.js
@@ -14,6 +14,25 @@ const s = new String('Hello World');
 
 x = typeof s;
 
-// x = s.length;
+x = s.length;
+
+// Access value by key
+x = s[1];
+
+x = s.__proto__;
+
+x = s.toUpperCase();
+x = s.toLowerCase();
+x = s.charAt(0);
+x = s.indexOf('e');
+x = s.substring(0, 5);
+x = s.substring(6);
+x = s.slice(-11);
+x = '    Hello World';
+x = s.trim();
+x = s.replace('World', 'John');
+x = s.includes('hello');
+x = s.valueOf();
+x = s.split('');
 
 console.log(x);
