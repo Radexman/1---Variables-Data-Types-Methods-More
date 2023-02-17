d8543acf15274f48541a62495b3b65fbd6329b0e
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2023-02-17 13:22:05 +0100

    02.17

--- 10 - capitalize-challange/script.js
+++ 10 - capitalize-challange/script.js
@@ -17,12 +17,42 @@
 // };
 
 // console.log(getRandomChar(myString));
-let amount = 1;
+// let amount = 1;
 // let x = parseInt(amount);
 // x = +amount;
 // x = Number(amount);
-// amount = String(amount);
 // amount = amount.toString();
 // amount = parseFloat(amount);
-amount = Boolean(amount);
-console.log(amount, typeof amount);
+// amount = Boolean(amount);
+// console.log(amount, typeof amount);
+// amount = String(amount);
+// let x;
+// let name = 'Emilia';
+// let age = 20;
+// x = 'Hello, my name is ' + name + ' and I am ' + age + ' years old';
+// x = `Hello, my name is ${name} and I'm ${age} years old`;
+// const s = new String('Hello World');
+// x = s.length;
+// x = typeof s;
+// x = s[0];
+// x = s.charAt(0);
+// x = s.__proto__;
+// x = s.toUpperCase();
+// x = s.toLowerCase();
+// x = s.charAt(0);
+// x = s.indexOf('H');
+// x = s.substring(1, 5);
+// x = s.substring(6);
+// x = s.includes('ello');
+// x = s.slice(-11, -6);
+// x = s.trim(' ');
+// x = s.replace('World', 'Emilia');
+// x = s.valueOf();
+// x = s.split(' ');
+// console.log(x, typeof x);
+
+let myString = 'developeR';
+const toPropperCase = (string) => {
+	return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+console.log(toPropperCase(myString));
