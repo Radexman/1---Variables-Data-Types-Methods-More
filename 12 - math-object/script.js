116375c607a9af477ef1d604103649e823b9f855
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2023-02-19 08:40:48 +0100

    02.19

---
+++ 12 - math-object/script.js
@@ -0,0 +1,12 @@
+let x;
+x = Math.sqrt(9);
+x = Math.abs(-5);
+x = Math.round(4.2);
+x = Math.ceil(4.1);
+x = Math.floor(4.9);
+x = Math.pow(2, 2);
+x = Math.min(4, 5, 1);
+x = Math.max(4, 5, 1);
+x = Math.random();
+x = Math.floor(Math.random() * 100 + 1);
+console.log(x, typeof x);
