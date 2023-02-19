04e3b4a3c293f8ce90f40823a0b50f210109c132
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2023-02-19 17:13:10 +0100

    02.19

---
+++ 15 - date-methods/script.js
@@ -0,0 +1,21 @@
+let x;
+let d = new Date();
+x = d.toString();
+x = d.getTime();
+x = d.valueOf();
+x = d.getFullYear();
+x = d.getMonth() + 1;
+x = d.getDate();
+x = d.getDay();
+x = d.getHours();
+x = d.getMinutes();
+x = d.getSeconds();
+x = d.getMilliseconds();
+x = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDay()}`;
+x = Intl.DateTimeFormat('en-US').format(d);
+x = Intl.DateTimeFormat('en-GB').format(d);
+x = Intl.DateTimeFormat('default').format(d);
+x = Intl.DateTimeFormat('default', { month: 'long' }).format(d);
+x = d.toLocaleString('default', { month: 'long' });
+
+console.log(x);
