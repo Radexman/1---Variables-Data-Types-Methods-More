5839c3934e2f4d35b209ebf7349472b64d22311d
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2023-02-19 08:23:59 +0100

    02.19

--- 11 - working-with-numbers/script.js
+++ 11 - working-with-numbers/script.js
@@ -5,4 +5,16 @@ x = num.toString().length;
 x = num.toFixed(2);
 x = num.toPrecision(3);
 x = num.toExponential(3);
+x = num.toLocaleString('pl');
+x = num.valueOf();
+x = Number.MAX_VALUE;
+x = Number.MIN_VALUE;
 console.log(x, typeof x);
+
+let num = new Number(10);
+let x;
+x = num.toString().length;
+x = num.toFixed(2);
+x = num.toPrecision(4);
+
+console.log(x);
